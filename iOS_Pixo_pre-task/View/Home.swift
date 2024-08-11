6396810dc05a705ca6e69f08f12d67c3b9324816
Hashwim <ss991117@naver.com>
2024-08-11 22:10:27 +0900

    [Feat] 상단 오버레이 타이틀 표시

--- iOS_Pixo_pre-task/View/Home.swift
+++ iOS_Pixo_pre-task/View/Home.swift
@@ -7,20 +7,38 @@
 import SwiftUI
 
 struct Home: View {
-
+    
     var size: CGSize
     var safeArea: EdgeInsets
     var sharedData = SharedData()
-
+    
     var body: some View {
         ScrollView(.vertical) {
             VStack(spacing: 10) {
                 //상단 캐러셀
                 PhotosScrollView(size: size, safeArea: safeArea)
-
+                
                 //하단 섹션
                 OtherContens()
             }
+        }
+        .overlay(alignment: .topLeading) {
+            HStack {
+                Text("Photos")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color.white)
+                    .padding(safeArea)
+                    .opacity(sharedData.activePage == 1 ? 1 : 0)
+
+                Spacer()
+                
+                Circle()
+                    .fill(.black)
+                    .frame(width: 20, height: 20)
+            }
+            .padding(.horizontal, 20)
+
         }
         .scrollDisabled(sharedData.isExpanded)
         .environment(sharedData)
